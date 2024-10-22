9be499d3bc67ace9c5c303e70469525fc9211b10
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-10-22 11:22:43 +0000

    chore: Update driver code to use method calls for list

--- main.go
+++ main.go
@@ -8,25 +8,25 @@ import (
 func main() {
 	list := linkedList.NewList()
 
-	linkedList.PrintLinkedList(list)
+	list.Print()
 
-	linkedList.Append(list, "Maxwell", 12, "Change the world")
-	linkedList.Append(list, "Batman", 32, "Clean up Gotham of crime")
+	list.Append("Maxwell", 12, "Change the world")
+	list.Append("Batman", 32, "Clean up Gotham of crime")
 
-	linkedList.Prepend(list, "Clark Kent", 43, "Make the world a better place")
+	list.Prepend("Clark Kent", 43, "Make the world a better place")
 
-	linkedList.PrintLinkedList(list)
+	list.Print()
 
-	node := linkedList.AtIndex(list, 1)
+	node := list.AtIndex(1)
 
-	linkedList.PrintNodeInfo(node)
+	node.PrintNodeInfo()
 
-	linkedList.DeleteAt(list, 5)
-	linkedList.PrintLinkedList(list)
+	list.DeleteAt(1)
+	list.Print()
 
-	linkedList.PrintReverse(list.Head)
+	list.PrintReverse()
 	fmt.Println("nil")
 
-	linkedList.InsertAt(list, 2, "Diana Prince", 1200, "Make man's world a better place")
-	linkedList.PrintLinkedList(list)
+	list.InsertAt(2, "Diana Prince", 1200, "Make man's world a better place")
+	list.Print()
 }
