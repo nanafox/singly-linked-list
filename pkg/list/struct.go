26a39b67393a8de885bd84c819b4c17cc0d89768
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-10-25 16:32:11 +0000

    doc: Add documentation to node and list structs

--- pkg/list/struct.go
+++ pkg/list/struct.go
@@ -4,7 +4,7 @@ import "github.com/nanafox/singly-linked-list/pkg/node"
 
 // List is the structure for the singly linked list
 type List struct {
-	Head *node.Node
-	Tail *node.Node
-	Size int
+	Head *node.Node // The head node of the list (denotes the beginning)
+	Tail *node.Node // the tail node of the list (denotes the end)
+	Size int        // Tracks the size of the list as it grows
 }
--- pkg/node/struct.go
+++ pkg/node/struct.go
@@ -1,8 +1,9 @@
 package node
 
+// Node defines the node structure for the linked list data structure
 type Node struct {
-	Name    string
-	Age     int
-	Passion string
-	Next    *Node
+	Name    string // The name of the person
+	Age     int    // The age of the person
+	Passion string // The person's innermost desire and passion
+	Next    *Node  // A pointer to the next node
 }
